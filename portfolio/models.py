5b6d95a1267dd1a716153870a30a05de12d43c73
students <darshanpatil761@gmail.com>
2019-02-05 14:03:00 +0530

    models added

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class User(models.models):
+class User(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=120)
+    portfolio_value = models.FloatField()
+
+class Stock(models.Model):
+    name = models.CharField(max_length=120)
+    qty = models.IntegerField()
+    company = models.CharField(max_length=120)
+    purchase_val = models.FloatField()
+    current_val = models.FloatField()
+
     
 
